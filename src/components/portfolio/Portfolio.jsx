cab71bde656252eaee69e17bb590f25f67982d1e
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-07-08 00:34:25 -0300

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,35 +6,35 @@ const items = [
   {
     id: 1,
     title: "React eCommerce",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    img: "https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/6628005b058bc100081ce1c7/screenshot_2024-04-23-18-40-11-0000.webp&fit=cover&h=500&w=800",
     desc: "Se trata de mis primeras practicas con React.js - Consiste en un ecommerce de lotes de terrenos en un barrio residencial llamado Mandalas de agua, El proyecto no está finalizado y solo se refiere al front-end del sitio.",
     link: "https://mandalasdeagua.netlify.app/",
   },
   {
     id: 2,
     title: "Editor Automata",
-    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    img: "https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/66873d3ae11c9d00080d7f35/screenshot_2024-07-05-00-24-48-0000.webp&fit=cover&h=500&w=800",
     desc: "Desarrollado con vanilla JS , el proyecto me fue util para practicar mis conocimientos en Css y Html - Consiste en un lienzo en el cual se pueden desplegar puntos y unir los mismo con flechas. luego al agregar entradas y salidas a las flechas y nombres a los puntos se tiene un grafico de grafos para representar automatas de estados finitos. El proyecto quedó en estado incompleto porque decidí comenzar de nuevo utilizando algun framework de JS",
     link: "https://ematijasevic-aef.netlify.app/",
   },
   {
     id: 3,
     title: "Página de presentación",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Fue la primera practica real que hice con JS , el proyecto consistía en un portafolio virtual - El mismo actualmente fue reemplazado por el sitio actual en cuestión",
+    img: "https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/6268c699d813ad000877da11/screenshot_2022-04-27-04-29-20-0000.png&fit=cover&h=500&w=800",
+    desc: "Fue la primera práctica real que hice con JS , el proyecto consistía en un portafolio virtual - El mismo actualmente fue reemplazado por el sitio actual en cuestión",
     link: "https://emersonmatijasevic.netlify.app/",
   },
   {
     id: 4,
     title: "Listado de Certificaciones",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/668376123ba29a0008bc33e2/screenshot_2024-07-02-03-38-12-0000.webp&fit=cover&h=500&w=800",
     desc: "El proyecto nació para practicar el consumo de APIs mediante Js y poder hacer uso de los endpoints que tenia desplegado. El mismo lista mediante una api todas las certificaciones de los cursos de programación que tomé a lo largo de mi carrera. El proyecto está terminado y realizo mantenimiento al mismo cada que sea necesario. Utilicé JS Css y Html debido a la sencillez del mismo.",
     link: "https://ematijasevic-certificados.netlify.app/",
   },
   {
     id: 5,
     title: "Ejemplo de Formulario",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/6672033fe3f41b00084d1f50/screenshot_2024-06-18-21-59-45-0000.webp&fit=cover&h=500&w=800",
     desc: "Ejemplo de formulario web con vanilla js . Trabajo relaizado como tarea de entrega del curso de programación fullstack en CILSA año 2024",
     link: "https://cilsa-ematijasevic-ejercicio1.netlify.app/",
   },
@@ -54,7 +54,9 @@ const Single = ({ item }) => {
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
-            <img src={item.img} alt="" />
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <img src={item.img} alt="" />
+            </a>
           </div>
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
