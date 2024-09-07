c4100b238a3d72794faff1a5e0e6def12c224d6d
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-09-06 21:00:12 -0300

    agrego trelloapp

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,11 +4,18 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
   {
-    id: 1000,
+    id: 999,
     title: "JAVA ApiRest Facturación",
     img: "https://i.imgur.com/GzW6tZw.png",
     desc: "Se trata de una API Rest con Java Spring Boot - Consiste en el backend un ecommerce genérico con Clientes , Ventas , Productos y demas entidades relevantes - En el link puede ingresar a la documentacion de la API. [ Java + Spring + Spring Boot + Maven ] [Railway]",
     link: "https://demo-apirest-production.up.railway.app/swagger-ui/index.html",
+  },
+  {
+    id: 1000,
+    title: "Trello App",
+    img: "https://d33wubrfki0l68.cloudfront.net/66db95be90eba43648bc67e9/screenshot_2024-09-06-23-53-07-0000.webp",
+    desc: "Se trata de una app estilo Trello/Jira para la gestión de proyecto. Producto del trabajo integrador final de un curso de desarrollo Fullstack impartido por CILSA [ JavaScript + React + MaterialUI + Vite ] [Node.js + Express + MySql + Netlify]",
+    link: "https://task-ban.netlify.app/",
   },
   {
     id: 1001,
