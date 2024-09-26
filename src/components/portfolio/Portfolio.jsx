2e37cb9b3fbf7e681be4f2a4162547ef4e04c637
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2024-09-25 22:19:04 -0300

    agregovendelapp

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,11 +4,20 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
   {
+    id: 998,
+    title: "Ruby on Rails - eCommerce",
+    img: "https://miro.medium.com/v2/resize:fit:820/1*y7ZgcnbxdF4aDgh-XaS1-Q.png",
+    desc: "Una de mis primeras prácticas con Ruby on Rails , se trata de un ecommerce de productos de segunda mano, con funcionalidad de login y gestion de productos. El desarrollo esta actualmente en progreso [ Ruby 3.3.5 + Rails 7 + SQLite + Turbo ] [Railway]",
+    link: "",
+    repo: "https://github.com/Matijasevic-Emer/vendelapp_v2",
+  },
+  {
     id: 999,
     title: "JAVA ApiRest Facturación",
     img: "https://i.imgur.com/GzW6tZw.png",
-    desc: "Se trata de una API Rest con Java Spring Boot - Consiste en el backend un ecommerce genérico con Clientes , Ventas , Productos y demas entidades relevantes - En el link puede ingresar a la documentacion de la API. [ Java + Spring + Spring Boot + Maven ] [Railway]",
+    desc: "API Rest con Java Spring Boot - Consiste en el backend un ecommerce genérico con Clientes , Ventas , Productos y demas entidades relevantes - En el link puede ingresar a la documentacion de la API. [ Java + Spring + Spring Boot + Maven ] [Railway]",
     link: "https://api-facturacion-production.up.railway.app/swagger-ui/index.html#",
+    repo: "https://github.com/Matijasevic-Emer/api-facturacion",
   },
   {
     id: 1000,
@@ -16,13 +25,15 @@ const items = [
     img: "https://d33wubrfki0l68.cloudfront.net/66db95be90eba43648bc67e9/screenshot_2024-09-06-23-53-07-0000.webp",
     desc: "Se trata de una app estilo Trello/Jira para la gestión de proyecto. Producto del trabajo integrador final de un curso de desarrollo Fullstack impartido por CILSA [ JavaScript + React + MaterialUI + Vite ] [Node.js + Express + MySql + Netlify]",
     link: "https://task-ban.netlify.app/",
+    repo: "https://github.com/Matijasevic-Emer/taskban",
   },
   {
     id: 1001,
     title: "React eCommerce",
     img: "https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/66ace1e487a7fa0008147067/screenshot_2024-08-02-13-41-50-0000.webp&fit=cover&h=500&w=800",
-    desc: "Se trata de mis primeras practicas con React.js - Consiste en un ecommerce de lotes de terrenos en un barrio residencial llamado Mandalas de agua. El proyecto está finalizado y solo es demostrativo (no para su uso comercial). Contiene un catalogo con carrusel de imagenes, un mapa con su version gratuita y una sección para comparar terrenos. [ React.js + Vite + material UI ] [Node.js + Netlify]",
+    desc: "Una de mis primeras practicas con React.js - Consiste en un ecommerce de lotes de terrenos en un barrio residencial llamado Mandalas de agua. El proyecto está finalizado y solo es demostrativo (no para su uso comercial). Contiene un catalogo con carrusel de imagenes, un mapa con su version gratuita y una sección para comparar terrenos. [ React.js + Vite + material UI ] [Node.js + Netlify]",
     link: "https://terrenosargentinos.netlify.app/",
+    repo: "https://github.com/Matijasevic-Emer/terrenos-argentinos",
   },
   {
     id: 2001,
@@ -30,6 +41,7 @@ const items = [
     img: "https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/66a85b7896f3010008481b23/screenshot_2024-07-30-03-18-34-0000.webp&fit=cover&h=500&w=800",
     desc: "Trabajo grupal integrador del curso Fullstack developer .Consiste en un ecommerce de articulos de PC&Gamming . El proyecto no está finalizado y solo se refiere al front-end del sitio.",
     link: "https://tp-grupo5-cilsa.netlify.app/",
+    repo: "https://github.com/Matijasevic-Emer/cilsa-tpi-grupos/tree/prd",
   },
   {
     id: 3001,
@@ -37,6 +49,7 @@ const items = [
     img: "https://app.netlify.com/.netlify/images?url=https://d33wubrfki0l68.cloudfront.net/668376123ba29a0008bc33e2/screenshot_2024-07-02-03-38-12-0000.webp&fit=cover&h=500&w=800",
     desc: "El proyecto nació para practicar el consumo de APIs mediante Js y poder hacer uso de los endpoints que tenia desplegado. El mismo lista mediante una api todas las certificaciones de los cursos de programación que tomé a lo largo de mi carrera. El proyecto está terminado y realizo mantenimiento al mismo cada que sea necesario. Utilicé JS Css y Html debido a la sencillez del mismo.",
     link: "https://ematijasevic-certificados.netlify.app/",
+    repo: "https://github.com/Matijasevic-Emer/certificates-frontend",
   },
   {
     id: 3002,
@@ -70,7 +83,6 @@ const items = [
 
 const Single = ({ item }) => {
   const ref = useRef();
-
   const { scrollYProgress } = useScroll({
     target: ref,
   });
@@ -88,10 +100,18 @@ const Single = ({ item }) => {
           </div>
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
+
             <p>{item.desc}</p>
-            <a href={item.link} target="_blank" rel="noopener noreferrer">
-              <button>Ver Demo</button>
-            </a>
+            <div className="button-group">
+              {item.repo && (
+                <a href={item.repo} target="_blank" rel="noopener noreferrer">
+                  <button className="boton">Ver Repo</button>
+                </a>
+              )}
+              <a href={item.link} target="_blank" rel="noopener noreferrer">
+                <button className="botonNotM">Ver Demo</button>
+              </a>
+            </div>
           </motion.div>
         </div>
       </div>
@@ -115,7 +135,7 @@ const Portfolio = () => {
   return (
     <div className="portfolio" ref={ref}>
       <div className="progress">
-        <h1>Proyectos no Genexus</h1>
+        <h2>Proyectos no Genexus</h2>
         <motion.div style={{ scaleX }} className="progressBar"></motion.div>
       </div>
       {items.map((item) => (
